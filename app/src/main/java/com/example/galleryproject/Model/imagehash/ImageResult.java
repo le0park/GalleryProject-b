b88eae20ed4c58eafc103d4acd94f895710dab3a
Junyoung Park <carpe0308@naver.com>
2019-11-03 17:27:32 +0900

    ImageHash (Average Hash, hamming distance) 구현

---
+++ app/src/main/java/com/example/galleryproject/Model/imagehash/ImageResult.java
@@ -0,0 +1,40 @@
+package com.example.galleryproject.Model.imagehash;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ * Created by samfch on 8/2/16.
+ */
+public class ImageResult implements Serializable, Comparable {
+    private String imgUrl;
+    private String percentSimilarity;
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getPercentSimilarity() {
+        return percentSimilarity;
+    }
+
+    public void setPercentSimilarity(String percentSimilarity) {
+        this.percentSimilarity = percentSimilarity;
+    }
+
+    @Override
+    public int compareTo(@NonNull Object o) {
+        int compareage = Integer.parseInt(((ImageResult) o).getPercentSimilarity());
+        /* For Ascending order*/
+//        return Integer.parseInt(this.percentSimilarity) - compareage;
+
+        /* For Descending order do like this */
+        return compareage - Integer.parseInt(this.percentSimilarity);
+    }
+}
